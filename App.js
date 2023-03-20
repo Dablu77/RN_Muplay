f0ac1902f69e40bf9d3262bf399e4fc51659cd11
dablu77 <dablugupta7739@gmail.com>
2023-03-20 15:23:21 +0530

    first commit

--- App.js
+++ App.js
@@ -1,27 +1,17 @@
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import AuthStack from './src/Navigation/StackNavigators/AuthStack';
-// import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthStack from './src/Navigation/StackNavigators/AuthStack';
+import 'react-native-gesture-handler';
 
 
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <AuthStack />
-//     </NavigationContainer>
-//   );
-// }
-
-// export default App
-
-
-import { View, Text } from 'react-native'
-import React from 'react'
-
-export default function App() {
+function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  );
 }
+
+export default App
+
+
